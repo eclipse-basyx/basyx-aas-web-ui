053f41263067d1f053cba9a6a9f2732937bdf9e7
Aaron Zielstorff <aaron.zi@web.de>
2024-09-13 20:36:18 +0200

    adds missing route imports in chart components

--- aas-web-ui/src/components/Widgets/Histogram.vue
+++ aas-web-ui/src/components/Widgets/Histogram.vue
@@ -36,6 +36,7 @@
 <script lang="ts">
     import _ from 'lodash';
     import { defineComponent } from 'vue';
+    import { useRoute } from 'vue-router';
     import { useTheme } from 'vuetify';
     import DashboardHandling from '@/mixins/DashboardHandling';
     import WidgetHandling from '@/mixins/WidgetHandling';
@@ -49,10 +50,12 @@
         setup() {
             const theme = useTheme();
             const navigationStore = useNavigationStore();
+            const route = useRoute();
 
             return {
                 theme, // Theme Object
                 navigationStore,
+                route, // Route Object
             };
         },
 
