342fb23897a8966a86b589ee687bc55f1d735af5
Sebastian Eicke <sebastian.eicke@HARTING.com>
2024-11-12 09:43:06 +0100

    Docker build with proxy (#84)
    * Docker build with proxy
    
    * Remove blank lines

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@ PRJ_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 FRESH_INSTALL='0'
 Yellow='\033[1;33m'
 ColorOff='\033[0m'
+# Specify proxy if needed
+# export HTTP_PROXY="http://proxyserver:port"
+# export HTTPS_PROXY="http://proxyserver:port"
 
 echo -e "${Yellow}Do a clean install?  (y/n) Default:n ${ColorOff}"
 read clean_install
