2a71704b2affb938b331788c2c3c026b6b944163
Sebastian Eicke <sebastian.eicke@HARTING.com>
2024-11-15 08:44:46 +0100

    Visualization titles: Usage of nameToDisplay() and add description

--- aas-web-ui/src/components/SubmodelPlugins/BillsOfMaterial.vue
+++ aas-web-ui/src/components/SubmodelPlugins/BillsOfMaterial.vue
@@ -3,8 +3,18 @@
         <!-- Header -->
         <v-card class="mb-4">
             <v-card-title>
-                <div class="text-subtitle-1">{{ 'Bills of Material:' }}</div>
+                <div class="text-subtitle-1">
+                    {{
+                        nameToDisplay(submodelElementData) &&
+                        nameToDisplay(submodelElementData) != submodelElementData.idShort
+                            ? nameToDisplay(submodelElementData)
+                            : 'Bills of Material:'
+                    }}
+                </div>
             </v-card-title>
+            <v-card-text v-if="descriptionToDisplay(submodelElementData)" class="pt-0">
+                {{ descriptionToDisplay(submodelElementData) }}
+            </v-card-text>
         </v-card>
         <!-- BoM Graph -->
         <v-card>
--- aas-web-ui/src/components/SubmodelPlugins/ContactInformation.vue
+++ aas-web-ui/src/components/SubmodelPlugins/ContactInformation.vue
@@ -3,8 +3,18 @@
         <!-- Header -->
         <v-card class="mb-4">
             <v-card-title>
-                <div class="text-subtitle-1">{{ 'Contact Information:' }}</div>
+                <div class="text-subtitle-1">
+                    {{
+                        nameToDisplay(submodelElementData) &&
+                        nameToDisplay(submodelElementData) != submodelElementData.idShort
+                            ? nameToDisplay(submodelElementData)
+                            : 'Contact Information:'
+                    }}
+                </div>
             </v-card-title>
+            <v-card-text v-if="descriptionToDisplay(submodelElementData)" class="pt-0">
+                {{ descriptionToDisplay(submodelElementData) }}
+            </v-card-text>
         </v-card>
         <!-- Documents -->
         <v-card v-if="loading">
