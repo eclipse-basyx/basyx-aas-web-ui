690c1349a23a7a7b3d16c62772d2d86ffc882d01
Sebastian Eicke <sebastian.eicke@HARTING.com>
2024-12-08 06:47:21 +0100

    Add check for valid URL

--- aas-web-ui/src/store/EnvironmentStore.ts
+++ aas-web-ui/src/store/EnvironmentStore.ts
@@ -65,7 +65,20 @@ export const useEnvStore = defineStore({
                 this.keycloakClientId = config.keycloakClientId;
                 this.endpointConfigAvailable =
                     config.endpointConfigAvailable === true || config.endpointConfigAvailable === 'true' ? true : false;
-                this.singleAasRedirect = config.singleAasRedirect;
+                if (config.singleAasRedirect) {
+                    // https://gist.github.com/dperini/729294
+                    const expression =
+                        /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+                    const regex = new RegExp(expression);
+                    if (config.singleAasRedirect.match(regex)) {
+                        // If valid URL
+                        console.log('VALID URL');
+                        this.singleAasRedirect = config.singleAasRedirect;
+                    } else {
+                        console.log('NONVALID URL');
+                        this.singleAasRedirect = '';
+                    }
+                }
             } catch (error) {
                 console.error('Error fetching config.json: ', error);
             }
