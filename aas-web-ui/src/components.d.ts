eclipse-basyx/basyx-aas-web-ui

commit 1f6f4a0d24d2255177c4beb687a13f317771fae9

Fixes title of AASTreeview/SubmodelList (#206)
* Fixes title of AASTreeview/SubmodelList

* Adds component for AAS logo (SVG)

* Refactors AAS logo to be used as icon in vuetify

* Improves card title template parts

* Adds missing end of file line

---------

Co-authored-by: Aaron Zielstorff <aaron.zi@web.de>

aas-web-ui/src/components.d.ts
@@ -10,6 +10,7 @@ declare module 'vue' {
     AAS: typeof import('./components/EditorComponents/AAS.vue')['default']
     AASList: typeof import('./components/AppNavigation/AASList.vue')['default']
     AASListDetails: typeof import('./components/AppNavigation/AASListDetails.vue')['default']
+    AASLogo: typeof import('./components/Logos/AASLogo.vue')['default']
     AASTreeview: typeof import('./components/AASTreeview.vue')['default']
     AdministrativeInformationElement: typeof import('./components/UIComponents/AdministrativeInformationElement.vue')['default']
     AnnotatedRelationshipElement: typeof import('./components/SubmodelElements/AnnotatedRelationshipElement.vue')['default']

aas-web-ui/src/assets/Icons/AASIcon.vue
@@ -1,0 +1,22 @@
+<template>
+    <i>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90 90" aria-hidden="true" focusable="false" role="img" class="v-icon__svg">
+            <g>
+                <g id="Layer" focusable="false">
+                    <g>
+                        <path d="M69.5,49.7c-5.6,0-9-2.8-9-7.6h6.3c0,1.9.9,2.8,2.9,2.8s2.5-.7,2.5-1.9-1-1.7-3.7-2.4c-4-1.1-7.6-2.3-7.6-7s3.2-6.9,8.6-6.9,8.4,2.4,8.4,7.2h-6.1c0-1.6-.9-2.5-2.6-2.5s-2.3.7-2.3,1.7,1,1.6,3.8,2.3c4.1,1,7.7,2.3,7.7,7.2,0,4.4-3.4,7.1-8.9,7.1M50.2,49.2l-1.1-4h-7l-1.1,4h-6.2l7-22h7.5l7,22h-6.1ZM25.3,49.2l-1.1-4h-7l-1.1,4h-6.1l7-22h7.5l7,22h-6.2ZM88.9,26.1l-6.5-7.2c-.8-.8-1.8-1.3-3-1.3H10.5c-1.1,0-2.2.5-3,1.3l-6.5,7.2c-.6.8-1,1.8-1,2.8v41.4c0,2.3,1.8,4.1,4,4.1h2.7c2.2,0,4-1.9,4-4.1v-5.5c0-1,.4-2,1.1-2.8l2.8-3.1c.8-.9,1.8-1.3,3-1.3h54.7c1.1,0,2.1.4,2.8,1.2l3.2,3.2c.8.8,1.2,1.8,1.2,2.9v5.3c0,2.3,1.8,4.1,4,4.1h2.5c2.2,0,4-1.9,4-4.1V28.9c0-1-.4-2-1.1-2.8" />
+                        <path d="M45.7,31.5c-.6,2.4-1.2,5-1.9,7.4l-.4,1.5h4.5l-.4-1.5c-.7-2.4-1.3-5-1.8-7.4" />
+                        <path d="M20.8,31.5c-.6,2.4-1.2,5-1.9,7.4l-.4,1.5h4.5l-.4-1.5c-.6-2.4-1.3-5-1.8-7.4" />
+                    </g>
+                </g>
+            </g>
+        </svg>
+    </i>
+</template>
+
+<style scoped>
+svg {
+    display: inline-block;
+    vertical-align: middle;
+}
+</style>

aas-web-ui/src/assets/Icons/customIcons.ts
@@ -1,0 +1,13 @@
+import type { IconProps,IconSet } from "vuetify";
+import { h } from "vue";
+import aasIcon from "./AASIcon.vue";
+
+const customSvgNameToComponent: any = {
+    aasIcon,
+};
+
+const customIcons: IconSet = {
+    component: (props: IconProps) => h(customSvgNameToComponent[props.icon as string]),
+};
+
+export { customIcons };

aas-web-ui/src/components/AASTreeview.vue
@@ -2,17 +2,14 @@
     <v-container fluid class="pa-0">
         <v-card color="rgba(0,0,0,0)" elevation="0">
             <v-card-title style="padding: 15px 16px 16px">
+                <div v-if="!selectedAAS || Object.keys(selectedAAS).length === 0">AAS Treeview</div>
+                <div v-else class="d-flex align-center">
+                    <v-icon icon="custom:aasIcon" color="primary" size="small" class="ml-2" />
+                    <span class="text-truncate ml-2">
+                        {{ nameToDisplay(selectedAAS) }}
+                    </span>
+                </div>
                 <!-- TODO: Add Searchfield to filter the Treeview -->
-                <v-row align="center">
-                    <v-col cols="auto">
-                        <span>AAS Treeview</span>
-                    </v-col>
-                    <v-col v-if="nameToDisplay(selectedAAS)" cols="auto" class="pl-1 pt-2">
-                        <v-chip size="x-small" color="primary" label border>{{
-                            'AAS: ' + nameToDisplay(selectedAAS)
-                        }}</v-chip>
-                    </v-col>
-                </v-row>
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text style="overflow-y: auto; height: calc(100vh - 170px)">

aas-web-ui/src/components/ComponentVisualization.vue
@@ -1,25 +1,15 @@
 <template>
     <v-container fluid class="pa-0">
         <v-card color="card" elevation="0">
-            <v-card-title v-if="!isMobile" style="padding: 15px 16px 16px"> Visualization </v-card-title>
-            <v-card-title v-else style="padding: 15px 16px 16px">
-                <v-row align="center">
-                    <v-col cols="auto" class="pa-0">
-                        <v-btn
-                            class="ml-2"
-                            variant="plain"
-                            icon="mdi-chevron-left"
-                            @click="backToSubmodelList()"></v-btn>
-                    </v-col>
-                    <v-col cols="auto">
-                        <span>Visualization</span>
-                    </v-col>
-                    <v-col v-if="nameToDisplay(selectedAAS)" cols="auto" class="pl-1">
-                        <v-chip size="x-small" color="primary" label border>{{
-                            'AAS: ' + nameToDisplay(selectedAAS)
-                        }}</v-chip>
-                    </v-col>
-                </v-row>
+            <v-card-title :style="{ padding: isMobile ? '' : '15px 16px 16px' }">
+                <div v-if="!isMobile">Visualization</div>
+                <div v-else class="d-flex align-center">
+                    <v-btn class="ml-0" variant="plain" icon="mdi-chevron-left" @click="backToSubmodelList()" />
+                    <v-icon icon="custom:aasIcon" color="primary" size="small" class="ml-2" />
+                    <span class="text-truncate ml-2">
+                        {{ nameToDisplay(selectedAAS) }}
+                    </span>
+                </div>
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text style="overflow-y: auto; height: calc(100svh - 170px)">
@@ -151,7 +141,7 @@
 
         // Sort filtered plugins with respect to semanticId
         plugins
-            .sort((pluginA, pluginB) => {
+            .sort((pluginA: any, pluginB: any) => {
                 let pluginASemanticId = '';
                 let pluginBSemanticId = '';
 
@@ -161,7 +151,7 @@
                 if (Array.isArray(pluginA.semanticId)) {
                     if (pluginA.semanticId.length > 0) {
                         pluginA.semanticId
-                            .sort((semanticIdA, semanticIdB) => semanticIdA.localeCompare(semanticIdB))
+                            .sort((semanticIdA: any, semanticIdB: any) => semanticIdA.localeCompare(semanticIdB))
                             .reverse();
                         pluginASemanticId = pluginA.semanticId[0];
                     }
@@ -170,7 +160,7 @@
                 if (Array.isArray(pluginB.semanticId)) {
                     if (pluginB.semanticId.length > 0) {
                         pluginB.semanticId
-                            .sort((semanticIdA, semanticIdB) => semanticIdA.localeCompare(semanticIdB))
+                            .sort((semanticIdA: any, semanticIdB: any) => semanticIdA.localeCompare(semanticIdB))
                             .reverse();
                         pluginBSemanticId = pluginB.semanticId[0];
                     }

aas-web-ui/src/components/SubmodelList.vue
@@ -1,20 +1,20 @@
 <template>
     <v-container fluid class="pa-0">
         <v-card color="card" elevation="0">
-            <v-card-title style="padding: 15px 16px 16px">
-                <v-row align="center">
-                    <v-col v-if="isMobile" cols="auto" class="pa-0">
-                        <v-btn class="ml-2" variant="plain" icon="mdi-chevron-left" @click="backToAASList()"></v-btn>
-                    </v-col>
-                    <v-col cols="auto">
-                        <span>Submodel List</span>
-                    </v-col>
-                    <v-col v-if="nameToDisplay(selectedAAS)" cols="auto" class="pl-1 pt-2">
-                        <v-chip size="x-small" color="primary" label border>{{
-                            'AAS: ' + nameToDisplay(selectedAAS)
-                        }}</v-chip>
-                    </v-col>
-                </v-row>
+            <v-card-title :style="{ padding: isMobile ? '' : '15px 16px 16px' }">
+                <div v-if="!selectedAAS || Object.keys(selectedAAS).length === 0">Submodel List</div>
+                <div v-else class="d-flex align-center">
+                    <v-btn
+                        v-if="isMobile"
+                        class="ml-0"
+                        variant="plain"
+                        icon="mdi-chevron-left"
+                        @click="backToAASList()" />
+                    <v-icon icon="custom:aasIcon" color="primary" size="small" class="ml-2" />
+                    <span class="text-truncate ml-2">
+                        {{ nameToDisplay(selectedAAS) }}
+                    </span>
+                </div>
             </v-card-title>
             <v-divider></v-divider>
             <v-card-text style="overflow-y: auto; height: calc(100svh - 170px)" class="py-2 px-2">

aas-web-ui/src/plugins/vuetify.ts
@@ -9,7 +9,9 @@ import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
 // Composables
 import { createVuetify } from 'vuetify';
+import { mdi } from 'vuetify/iconsets/mdi';
 import { VFileUpload } from 'vuetify/labs/VFileUpload';
+import { customIcons } from '@/assets/Icons/customIcons';
 import { adjustColorBrightness } from '@/utils/ThemeUtils';
 
 export function initializeVuetify(primaryLightColor: string, primaryDarkColor: string) {
@@ -21,6 +23,13 @@ export function initializeVuetify(primaryLightColor: string, primaryDarkColor: s
     const vuetify = createVuetify({
         components: {
             VFileUpload,
+        },
+        icons: {
+            defaultSet: 'mdi',
+            sets: {
+                mdi,
+                custom: customIcons,
+            },
         },
         theme: {
             themes: {

commit d2cd3678fe9ac146c9209277e9509f12e2186e11

Fix components.d.ts (caused by PR #206)

aas-web-ui/src/components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
     AAS: typeof import('./components/EditorComponents/AAS.vue')['default']
     AASList: typeof import('./components/AppNavigation/AASList.vue')['default']
     AASListDetails: typeof import('./components/AppNavigation/AASListDetails.vue')['default']
-    AASLogo: typeof import('./components/Logos/AASLogo.vue')['default']
     AASTreeview: typeof import('./components/AASTreeview.vue')['default']
     AdministrativeInformationElement: typeof import('./components/UIComponents/AdministrativeInformationElement.vue')['default']
     AnnotatedRelationshipElement: typeof import('./components/SubmodelElements/AnnotatedRelationshipElement.vue')['default']