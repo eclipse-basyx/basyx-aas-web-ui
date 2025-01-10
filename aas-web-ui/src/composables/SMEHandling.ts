542ee5ca6123fbfe351ca381cd3b43389fa80659
Sebastian Eicke <sebastian.eicke@HARTING.com>
2025-01-10 05:03:15 +0100

    Improves SM/SMEHandling

--- aas-web-ui/src/composables/SMEHandling.ts
+++ aas-web-ui/src/composables/SMEHandling.ts
@@ -2,11 +2,13 @@ import { useAASStore } from '@/store/AASDataStore';
 import { formatDate } from '@/utils/DateUtils';
 import { useSMRepositoryClient } from './Client/SMRepositoryClient';
 import { useConceptDescriptionHandling } from './ConceptDescriptionHandling';
+import { useSMHandling } from './SMHandling';
 
 export function useSMEHandling() {
     // Composables
     const { fetchSme } = useSMRepositoryClient();
     const { getConceptDescriptions } = useConceptDescriptionHandling();
+    const { fetchAndDispatchSm } = useSMHandling();
 
     // Stores
     const aasStore = useAASStore();
@@ -16,6 +18,12 @@ export function useSMEHandling() {
         // console.log('fetchAndDispatchSme()', submodelElementPath);
 
         if (submodelElementPath.trim() === '') return;
+
+        if (!submodelElementPath.includes('/submodel-elements/')) {
+            // No valid SME path, maybe just SM endpoint
+            fetchAndDispatchSm(submodelElementPath, withConceptDescriptions);
+            return;
+        }
 
         const sme = await fetchSme(submodelElementPath);
         sme.timestamp = formatDate(new Date());
--- aas-web-ui/src/composables/SMHandling.ts
+++ aas-web-ui/src/composables/SMHandling.ts
@@ -1,10 +1,14 @@
 import { useAASStore } from '@/store/AASDataStore';
 import { formatDate } from '@/utils/DateUtils';
 import { useSMRepositoryClient } from './Client/SMRepositoryClient';
+import { useConceptDescriptionHandling } from './ConceptDescriptionHandling';
+import { useSMEHandling } from './SMEHandling';
 
 export function useSMHandling() {
     // Composables
     const { fetchSm } = useSMRepositoryClient();
+    const { getConceptDescriptions } = useConceptDescriptionHandling();
+    const { fetchAndDispatchSme } = useSMEHandling();
 
     // Stores
     const aasStore = useAASStore();
@@ -15,13 +19,19 @@ export function useSMHandling() {
 
         if (smEndpoint.trim() === '') return;
 
+        if (smEndpoint.includes('/submodel-elements/')) {
+            // smEndoint seems to be an SME endpoint
+            fetchAndDispatchSme(smEndpoint, withConceptDescriptions);
+            return;
+        }
+
         const sm = await fetchSm(smEndpoint);
         sm.timestamp = formatDate(new Date());
         sm.path = smEndpoint;
         sm.isActive = true;
 
         if (withConceptDescriptions) {
-            sm.conceptDescriptions = await getConceptDescriptions(selectedNode.value);
+            sm.conceptDescriptions = await getConceptDescriptions(sm.value);
         }
 
         aasStore.dispatchSelectedNode(sm);
