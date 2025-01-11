f637ce2728ad4421c90a4654174187d681f4f15b
Sebastian Eicke <sebastian.eicke@HARTING.com>
2025-01-11 07:02:59 +0100

    Adds ClipboardUtil composable

---
+++ aas-web-ui/src/composables/ClipboardUtil.ts
@@ -0,0 +1,31 @@
+import { useNavigationStore } from '@/store/NavigationStore';
+
+export function useClipboardUtil() {
+    // Store
+    const navigationStore = useNavigationStore();
+
+    function copyToClipboard(value: string, valueName: string, iconReference: { value: string }): void {
+        if (!value) return;
+
+        iconReference.value = 'mdi-check';
+
+        // copy value to clipboard
+        navigator.clipboard.writeText(value);
+
+        // set the clipboard tooltip to false after 1.5 seconds
+        setTimeout(() => {
+            iconReference.value = 'mdi-clipboard-file-outline';
+        }, 2000);
+
+        // open Snackbar to inform the user that the path was copied to the clipboard
+        navigationStore.dispatchSnackbar({
+            status: true,
+            timeout: 2000,
+            color: 'success',
+            btnColor: 'buttonText',
+            text: (valueName.trim() !== '' ? valueName : "'" + value + "'") + ' copied to Clipboard.',
+        });
+    }
+
+    return { copyToClipboard };
+}
