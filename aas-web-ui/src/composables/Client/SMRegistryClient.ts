c52721be683d19b38385264306d8232c9cc66d03
Christian Glomb <christian.glomb@gmail.com>
2025-01-17 16:18:31 +0100

    Update SMRegistryClient.ts

--- aas-web-ui/src/composables/Client/SMRegistryClient.ts
+++ aas-web-ui/src/composables/Client/SMRegistryClient.ts
@@ -7,126 +7,126 @@ import { base64Encode } from '@/utils/EncodeDecodeUtils';
 import { removeNullValues } from '@/utils/generalUtils';
 
 export function useSMRegistryClient() {
-  const { getRequest, postRequest, putRequest } = useRequestHandling();
+    const { getRequest, postRequest, putRequest } = useRequestHandling();
 
-  const navigationStore = useNavigationStore();
+    const navigationStore = useNavigationStore();
 
-  const submodelRegistryUrl = computed(() => navigationStore.getSubmodelRegistryURL);
+    const submodelRegistryUrl = computed(() => navigationStore.getSubmodelRegistryURL);
 
-  // Fetch List of all available SM Descriptors
-  async function fetchSmDescriptorList(): Promise<Array<any>> {
-    const failResponse = [] as Array<any>;
+    // Fetch List of all available SM Descriptors
+    async function fetchSmDescriptorList(): Promise<Array<any>> {
+        const failResponse = [] as Array<any>;
 
-    let smRegistryUrl = submodelRegistryUrl.value;
-    if (smRegistryUrl.trim() === '') return failResponse;
-    if (!smRegistryUrl.includes('/submodel-descriptors')) {
-      smRegistryUrl += '/submodel-descriptors';
+        let smRegistryUrl = submodelRegistryUrl.value;
+        if (smRegistryUrl.trim() === '') return failResponse;
+        if (!smRegistryUrl.includes('/submodel-descriptors')) {
+            smRegistryUrl += '/submodel-descriptors';
+        }
+
+        const smRegistryPath = smRegistryUrl;
+        const smRegistryContext = 'retrieving all SM Descriptors';
+        const disableMessage = false;
+        try {
+            const smRegistryResponse = await getRequest(smRegistryPath, smRegistryContext, disableMessage);
+            if (
+                smRegistryResponse.success &&
+                smRegistryResponse.data.result &&
+                smRegistryResponse.data.result.length > 0
+            ) {
+                return smRegistryResponse.data.result;
+            }
+        } catch {
+            // handle error
+            return failResponse;
+        }
+        return failResponse;
     }
 
-    const smRegistryPath = smRegistryUrl;
-    const smRegistryContext = 'retrieving all SM Descriptors';
-    const disableMessage = false;
-    try {
-      const smRegistryResponse = await getRequest(smRegistryPath, smRegistryContext, disableMessage);
-      if (
-        smRegistryResponse.success &&
-        smRegistryResponse.data.result &&
-        smRegistryResponse.data.result.length > 0
-      ) {
-        return smRegistryResponse.data.result;
-      }
-    } catch {
-      // handle error
-      return failResponse;
-    }
-    return failResponse;
-  }
+    // Fetch SM Descriptor by SM ID with SM Registry
+    async function fetchSmDescriptorById(smId: string): Promise<any> {
+        const failResponse = {} as any;
 
-  // Fetch SM Descriptor by SM ID with SM Registry
-  async function fetchSmDescriptorById(smId: string): Promise<any> {
-    const failResponse = {} as any;
+        let smRegistryUrl = submodelRegistryUrl.value;
+        if (smRegistryUrl.trim() === '') return failResponse;
+        if (!smRegistryUrl.includes('/submodel-descriptors')) {
+            smRegistryUrl += '/submodel-descriptors';
+        }
 
-    let smRegistryUrl = submodelRegistryUrl.value;
-    if (smRegistryUrl.trim() === '') return failResponse;
-    if (!smRegistryUrl.includes('/shell-descriptors')) {
-      smRegistryUrl += '/shell-descriptors';
-    }
-
-    const smRegistryPath = smRegistryUrl + '/' + base64Encode(smId).replace(/%3D/g, '');
-    const smRegistryContext = 'retrieving SM Descriptor';
-    const disableMessage = false;
-    try {
-      const smRegistryResponse = await getRequest(smRegistryPath, smRegistryContext, disableMessage);
-      if (
-        smRegistryResponse?.success &&
-        smRegistryResponse?.data &&
-        Object.keys(smRegistryResponse?.data).length > 0
-      ) {
-        return smRegistryResponse.data;
-      }
-    } catch {
-      // handle error
-      return failResponse;
-    }
-    return failResponse;
-  }
-
-  async function postSubmodelDescriptor(submodelDescriptor: descriptorTypes.SubmodelDescriptor): Promise<void> {
-    let submodelRegUrl = submodelRegistryUrl.value;
-    if (!submodelRegUrl.includes('/submodel-descriptors')) {
-      submodelRegUrl += '/submodel-descriptors';
+        const smRegistryPath = smRegistryUrl + '/' + base64Encode(smId);
+        const smRegistryContext = 'retrieving SM Descriptor';
+        const disableMessage = false;
+        try {
+            const smRegistryResponse = await getRequest(smRegistryPath, smRegistryContext, disableMessage);
+            if (
+                smRegistryResponse?.success &&
+                smRegistryResponse?.data &&
+                Object.keys(smRegistryResponse?.data).length > 0
+            ) {
+                return smRegistryResponse.data;
+            }
+        } catch {
+            // handle error
+            return failResponse;
+        }
+        return failResponse;
     }
 
-    const context = 'updating Submodel Descriptor';
-    const disableMessage = false;
-    const path = submodelRegUrl;
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    const body = JSON.stringify(submodelDescriptor);
+    async function postSubmodelDescriptor(submodelDescriptor: descriptorTypes.SubmodelDescriptor): Promise<void> {
+        let submodelRegUrl = submodelRegistryUrl.value;
+        if (!submodelRegUrl.includes('/submodel-descriptors')) {
+            submodelRegUrl += '/submodel-descriptors';
+        }
 
-    await postRequest(path, body, headers, context, disableMessage);
-  }
+        const context = 'updating Submodel Descriptor';
+        const disableMessage = false;
+        const path = submodelRegUrl;
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        const body = JSON.stringify(submodelDescriptor);
 
-  async function putSubmodelDescriptor(submodelDescriptor: descriptorTypes.SubmodelDescriptor): Promise<void> {
-    let submodelRegUrl = submodelRegistryUrl.value;
-    if (!submodelRegUrl.includes('/submodel-descriptors')) {
-      submodelRegUrl += '/submodel-descriptors';
+        await postRequest(path, body, headers, context, disableMessage);
     }
 
-    const context = 'updating Submodel Descriptor';
-    const disableMessage = false;
-    const path = submodelRegUrl + '/' + base64Encode(submodelDescriptor.id);
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    const body = JSON.stringify(submodelDescriptor);
+    async function putSubmodelDescriptor(submodelDescriptor: descriptorTypes.SubmodelDescriptor): Promise<void> {
+        let submodelRegUrl = submodelRegistryUrl.value;
+        if (!submodelRegUrl.includes('/submodel-descriptors')) {
+            submodelRegUrl += '/submodel-descriptors';
+        }
 
-    await putRequest(path, body, headers, context, disableMessage);
-  }
+        const context = 'updating Submodel Descriptor';
+        const disableMessage = false;
+        const path = submodelRegUrl + '/' + base64Encode(submodelDescriptor.id);
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        const body = JSON.stringify(submodelDescriptor);
 
-  function createDescriptorFromSubmodel(
-    submodel: jsonization.JsonObject,
-    endpoints: Array<descriptorTypes.Endpoint>
-  ): descriptorTypes.SubmodelDescriptor {
-    const jsonSubmodel = JSON.stringify(submodel);
-    const parsedSubmodel = JSON.parse(jsonSubmodel);
-    let descriptor = new descriptorTypes.SubmodelDescriptor(
-      endpoints,
-      parsedSubmodel.id,
-      parsedSubmodel.administration,
-      parsedSubmodel.description,
-      parsedSubmodel.displayName,
-      parsedSubmodel.extensions,
-      parsedSubmodel.idShort
-    );
-    descriptor = removeNullValues(descriptor);
-    return descriptor;
-  }
+        await putRequest(path, body, headers, context, disableMessage);
+    }
 
-  return {
-    fetchSmDescriptorList,
-    fetchSmDescriptorById,
-    postSubmodelDescriptor,
-    putSubmodelDescriptor,
-    createDescriptorFromSubmodel,
-  };
+    function createDescriptorFromSubmodel(
+        submodel: jsonization.JsonObject,
+        endpoints: Array<descriptorTypes.Endpoint>
+    ): descriptorTypes.SubmodelDescriptor {
+        const jsonSubmodel = JSON.stringify(submodel);
+        const parsedSubmodel = JSON.parse(jsonSubmodel);
+        let descriptor = new descriptorTypes.SubmodelDescriptor(
+            endpoints,
+            parsedSubmodel.id,
+            parsedSubmodel.administration,
+            parsedSubmodel.description,
+            parsedSubmodel.displayName,
+            parsedSubmodel.extensions,
+            parsedSubmodel.idShort
+        );
+        descriptor = removeNullValues(descriptor);
+        return descriptor;
+    }
+
+    return {
+        fetchSmDescriptorList,
+        fetchSmDescriptorById,
+        postSubmodelDescriptor,
+        putSubmodelDescriptor,
+        createDescriptorFromSubmodel,
+    };
 }
