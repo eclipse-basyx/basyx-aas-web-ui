7511409e444b917d49167a6fae94182e0390591b
Christian Glomb <christian.glomb@gmail.com>
2025-01-17 16:09:54 +0100

    Update EncodeDecodeUtils.ts

--- aas-web-ui/src/utils/EncodeDecodeUtils.ts
+++ aas-web-ui/src/utils/EncodeDecodeUtils.ts
@@ -1,4 +1,4 @@
-export function URLEncode(aasId: string) {
+export function base64Encode(aasId: string) {
     const base64Id = btoa(unescape(encodeURIComponent(aasId)));
     const urlSafeBase64Id = base64Id.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '%3D');
     return urlSafeBase64Id;
