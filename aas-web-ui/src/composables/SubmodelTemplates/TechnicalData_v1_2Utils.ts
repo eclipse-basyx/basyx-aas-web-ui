09feb429b0fc321369a17b19cfe77d9b1e93effa
Sebastian Eicke <sebastian.eicke@HARTING.com>
2025-01-21 17:57:16 +0100

    Updates composable for SMT TechnicalData v1.2

--- aas-web-ui/src/composables/SubmodelTemplates/TechnicalData_v1_2Utils.ts
+++ aas-web-ui/src/composables/SubmodelTemplates/TechnicalData_v1_2Utils.ts
@@ -1,11 +1,16 @@
 import { useAASRepositoryClient } from '@/composables/Client/AASRepositoryClient';
+import { useSMEHandling } from '@/composables/SMEHandling';
+import { useSMHandling } from '@/composables/SMHandling';
+import { useSMEFile } from '@/composables/SubmodelElements/File';
 import { extractId as extractIdFromReference } from '@/utils/ReferenceUtils';
-import { useSMRegistryClient } from '../Client/SMRegistryClient';
+import { checkSemanticId } from '@/utils/SemanticIdUtils';
 
 export function useTechnicalData_v1_2Utils() {
     // Composables
     const { getSubmodelRefsById: getSubmodelRefsByIdFromRepo } = useAASRepositoryClient();
-    const { fetchSmDescriptorById } = useSMRegistryClient();
+    const { fetchSmDescriptor, fetchSmById, getSmEndpointById } = useSMHandling();
+    const { fetchSme } = useSMEHandling();
+    const { valueUrl: smeFileValueUrl } = useSMEFile();
 
     const semanticId = 'https://admin-shell.io/ZVEI/TechnicalData/Submodel/1/2';
 
@@ -19,32 +24,31 @@ export function useTechnicalData_v1_2Utils() {
     //     return failResponse;
     // }
 
-    async function getSmTechnicalDataByAasId(aasId: string): Promise<any> {
-        console.log('getSmTechnicalDataByAasId()');
-        const failResponse = {};
+    async function getSmTechnicalDataId(aasId: string): Promise<string> {
+        const failResponse = '';
 
-        if (!aasId || aasId.trim() === '') return failResponse;
+        if (!aasId) return failResponse;
 
         aasId = aasId.trim();
+
+        if (aasId === '') return failResponse;
 
         const submodelRefs = await getSubmodelRefsByIdFromRepo(aasId);
 
         for (const submodelRef of submodelRefs) {
             const smId = extractIdFromReference(submodelRef, 'Submodel');
-            console.log(smId);
-            const smDescriptor = await fetchSmDescriptorById(smId);
+            const smDescriptor = await fetchSmDescriptor(smId);
             if (
                 smDescriptor &&
                 Object.keys(smDescriptor).length > 0 &&
-                smDescriptor.semanticIds &&
-                Array.isArray(smDescriptor.semanticIds) &&
-                smDescriptor.semanticIds.length > 0
+                smDescriptor?.semanticId?.keys &&
+                Array.isArray(smDescriptor.semanticId.keys) &&
+                smDescriptor.semanticId.keys.length > 0
             ) {
-                const semanticIds = smDescriptor.semanticId;
-                const foundSemanticId = semanticIds.includes((semanticIdOfSmDescriptor: any) => {
-                    return semanticIdOfSmDescriptor?.value === semanticId;
-                });
-                console.log('foobar', foundSemanticId);
+                const semanticIds = smDescriptor.semanticId.keys.map((key: any) => key.value);
+                if (semanticIds.includes(semanticId)) {
+                    return smId;
+                }
             }
         }
 
@@ -52,16 +56,58 @@ export function useTechnicalData_v1_2Utils() {
     }
 
     async function getProductImageUrl(aasId: string): Promise<string> {
-        console.log('getSmTechnicalDataByAasId()');
         const failResponse = '';
+
+        if (!aasId) return failResponse;
+
+        aasId = aasId.trim();
+
+        if (aasId === '') return failResponse;
+
+        // document.documentVersions = document.value.filter((element: any) => {
+        //     return (
+        //         element.semanticId &&
+        //         element.semanticId.keys &&
+        //         element.semanticId.keys.length > 0 &&
+        //         this.checkSemanticId(element, '0173-1#02-ABI503#001/0173-1#01-AHF582#001')
+        //     );
+        // });
 
         if (!aasId || aasId.trim() === '') return failResponse;
 
         aasId = aasId.trim();
 
-        const smTechnicalData = await getSmTechnicalDataByAasId(aasId);
-        return failResponse;
-        console.log(smTechnicalData);
+        // First attempt directly via smePath (smId => smEndpoint + SmePathSuffix (including idShorts))
+        const fileProductImageSmePathSuffix = 'GeneralInformation.ProductImage';
+
+        const smTechnicalDataId = await getSmTechnicalDataId(aasId);
+        const smTechnicalDataEndpoint = await getSmEndpointById(smTechnicalDataId);
+        const fileProductImageSmePath = smTechnicalDataEndpoint + '/submodel-elements/' + fileProductImageSmePathSuffix;
+        const fileProductImage = await fetchSme(fileProductImageSmePath);
+
+        if (fileProductImage && Object.keys(fileProductImage).length > 0) {
+            return smeFileValueUrl(fileProductImage);
+        } else {
+            // Second attempt via semanticIds
+            const smcGeneralInformationSemanticId = 'https://admin-shell.io/ZVEI/TechnicalData/GeneralInformation/1/1';
+            const fileProductImageSemanticId = 'https://admin-shell.io/ZVEI/TechnicalData/ProductImage/1/1';
+
+            const smTechnicalData = await fetchSmById(smTechnicalDataId);
+
+            if (smTechnicalData && Object.keys(smTechnicalData).length > 0) {
+                const smcGeneralInformation = smTechnicalData.submodelElements.find((sme: any) => {
+                    return checkSemanticId(sme, smcGeneralInformationSemanticId);
+                });
+                if (smcGeneralInformation && Object.keys(smcGeneralInformation).length > 0) {
+                    const fileProductImage = smcGeneralInformation.value.find((sme: any) => {
+                        return checkSemanticId(sme, fileProductImageSemanticId);
+                    });
+                    if (fileProductImage && Object.keys(fileProductImage).length > 0) {
+                        return smeFileValueUrl(fileProductImage);
+                    }
+                }
+            }
+        }
 
         return failResponse;
     }
