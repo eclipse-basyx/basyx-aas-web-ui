ddad3f966a445b5d662b57f9f0cb21a5991c1847
Sebastian Eicke <sebastian.eicke@HARTING.com>
2025-01-31 05:09:38 +0100

    Adds JSDoc, fixes comments

--- aas-web-ui/src/composables/VirtualContactFileUtils.ts
+++ aas-web-ui/src/composables/VirtualContactFileUtils.ts
@@ -8,8 +8,25 @@ export function useVirtualContactFileUtils() {
     const isMac = computed(() => navigationStore.getPlatform.mac);
     const isIOs = computed(() => navigationStore.getPlatform.ios);
 
-    function downloadVCard(vCardString: string, filename: string) {
+    /**
+     * Downloads a vCard file with the specified filename.
+     *
+     * This function checks if the provided string is a valid vCard format (starts with 'BEGIN:VCARD'
+     * and ends with 'END:VCARD'). If valid, it creates a Blob from the string, generates a URL
+     * for that Blob, and then initiates a download of the file. On iOS and Mac devices, it opens
+     * the vCard in a new tab instead of downloading it directly.
+     *
+     * @param {string} vCardString - The vCard data string, which must be in valid vCard format.
+     * @param {string} filename - The desired filename for the downloaded vCard file.
+     * @returns {void} This function does not return a value.
+     */
+    function downloadVCard(vCardString: string, filename: string): void {
         if (vCardString.startsWith('BEGIN:VCARD') && vCardString.endsWith('END:VCARD')) {
+            // Add file extension
+            if (!filename.toLocaleLowerCase().endsWith('.vcf')) {
+                filename = filename + '.vcf';
+            }
+
             const blob = new Blob([vCardString], { type: 'text/vcard;charset=utf-8;' });
             const data = window.URL.createObjectURL(blob);
             const link = document.createElement('a');
@@ -30,5 +47,6 @@ export function useVirtualContactFileUtils() {
             }, 100);
         }
     }
+
     return { downloadVCard };
 }
