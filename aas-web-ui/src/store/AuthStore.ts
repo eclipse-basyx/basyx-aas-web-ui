afa7e987d4f040fc3645e27abe941a72379260f7
Sebastian Eicke <sebastian.eicke@HARTING.com>
2025-03-05 10:45:36 +0100

    Work on stores (#351)
    * Avoids use of deprecated defineStore({})
    Usage of defineStore(id, options) instead
    
    * Deletes unnecessary vue import
    Deletes direct access to `singleAas` state
    
    * Harmonizes store comments
    
    * Deletes further unnecessary vue import

--- aas-web-ui/src/store/AuthStore.ts
+++ aas-web-ui/src/store/AuthStore.ts
@@ -1,42 +1,63 @@
 import Keycloak from 'keycloak-js';
 import { defineStore } from 'pinia';
 
-export const useAuthStore = defineStore({
-    id: 'authStore',
-    state: () => ({
-        token: '' as string | undefined,
-        refreshToken: '' as string | undefined,
-        authStatus: false as boolean,
-        authEnabled: false as boolean,
-        keycloak: null as Keycloak | null,
-        refreshIntervalId: undefined as number | undefined,
-    }),
-    getters: {
-        getToken: (state) => state.token,
-        getRefreshToken: (state) => state.refreshToken,
-        getAuthStatus: (state) => state.authStatus,
-        getAuthEnabled: (state) => state.authEnabled,
-        getKeycloak: (state) => state.keycloak,
-        getRefreshIntervalId: (state) => state.refreshIntervalId,
-    },
-    actions: {
-        setToken(token: string | undefined) {
-            this.token = token;
-        },
-        setRefreshToken(refreshToken: string | undefined) {
-            this.refreshToken = refreshToken;
-        },
-        setAuthStatus(authStatus: boolean) {
-            this.authStatus = authStatus;
-        },
-        setAuthEnabled(authEnabled: boolean) {
-            this.authEnabled = authEnabled;
-        },
-        setKeycloak(keycloak: Keycloak | null) {
-            this.keycloak = keycloak;
-        },
-        setRefreshIntervalId(intervalId: number) {
-            this.refreshIntervalId = intervalId;
-        },
-    },
+export const useAuthStore = defineStore('authStore', () => {
+    // States
+    const token = ref('' as string | undefined);
+    const refreshToken = ref('' as string | undefined);
+    const authStatus = ref(false);
+    const authEnabled = ref(false);
+    const keycloak = ref(null as Keycloak | null);
+    const refreshIntervalId = ref(undefined as number | undefined);
+
+    // Getters
+    const getToken = computed(() => token.value);
+    const getRefreshToken = computed(() => refreshToken.value);
+    const getAuthStatus = computed(() => authStatus.value);
+    const getAuthEnabled = computed(() => authEnabled.value);
+    const getKeycloak = computed(() => keycloak.value);
+    const getRefreshIntervalId = computed(() => refreshIntervalId.value);
+
+    // Actions
+    function setToken(tokenValue: string | undefined): void {
+        token.value = tokenValue;
+    }
+
+    function setRefreshToken(refreshTokenValue: string | undefined): void {
+        refreshToken.value = refreshTokenValue;
+    }
+
+    function setAuthStatus(authStatusValue: boolean): void {
+        authStatus.value = authStatusValue;
+    }
+
+    function setAuthEnabled(authEnabledValue: boolean): void {
+        authEnabled.value = authEnabledValue;
+    }
+
+    function setKeycloak(keycloakValue: Keycloak | null): void {
+        keycloak.value = keycloakValue;
+    }
+
+    function setRefreshIntervalId(refreshIntervalIdValue: number): void {
+        refreshIntervalId.value = refreshIntervalIdValue;
+    }
+
+    return {
+        // Getters
+        getToken,
+        getRefreshToken,
+        getAuthStatus,
+        getAuthEnabled,
+        getKeycloak,
+        getRefreshIntervalId,
+
+        // Actions
+        setToken,
+        setRefreshToken,
+        setAuthStatus,
+        setAuthEnabled,
+        setKeycloak,
+        setRefreshIntervalId,
+    };
 });
