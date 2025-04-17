50b817ee2e1e6381c05c5b738c892084eedf9141
FriedJannik <jannik.fried@iese.fraunhofer.de>
2025-04-17 10:39:02 +0200

    Removes unnecessary css classes
    Signed-off-by: FriedJannik <jannik.fried@iese.fraunhofer.de>

--- aas-web-ui/src/components/UIComponents/VTreeview.vue
+++ aas-web-ui/src/components/UIComponents/VTreeview.vue
@@ -340,14 +340,6 @@
 </script>
 
 <style scoped>
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: opacity 0.3s ease;
-    }
-    .fade-enter-from,
-    .fade-leave-to {
-        opacity: 0;
-    }
     .icon-placeholder {
         width: 24px;
         height: 24px;
