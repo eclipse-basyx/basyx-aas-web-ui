59edbc9f33dc4fc89f4cbe670f8a40923c17b836
Aaron Zielstorff <aaron.zi@web.de>
2025-06-17 16:05:02 +0200

    Improves user menu

--- aas-web-ui/src/components/AppNavigation/User.vue
+++ aas-web-ui/src/components/AppNavigation/User.vue
@@ -1,22 +1,24 @@
 <template>
     <v-menu :close-on-content-click="false" location="bottom">
-        <template #activator="{ props }">
+        <template #activator="{ props: menuProps }">
             <v-btn
-                v-bind="props"
-                :icon="isAuthEnabled ? (authStatus ? 'mdi-account-lock' : 'mdi-lock-remove') : 'mdi-lock-off'"></v-btn>
+                v-if="isAuthEnabled"
+                v-bind="menuProps"
+                :icon="authStatus ? 'mdi-account-lock' : 'mdi-lock-remove'"></v-btn>
+            <v-tooltip v-else text="Authorization Status" location="bottom" :open-delay="600">
+                <template #activator="{ props: tooltipProps }">
+                    <v-icon v-bind="tooltipProps">mdi-lock-off</v-icon>
+                </template>
+                <span>Authentication disabled</span>
+            </v-tooltip>
         </template>
         <v-card
             v-if="isAuthEnabled"
             min-width="300px"
             color="navigationMenu"
             style="border-style: solid; border-width: 1px">
-            <v-list nav class="pb-0 bg-navigationMenu">
-                <v-list-item
-                    class="pt-2"
-                    :active="false"
-                    nav
-                    :subtitle="authStatus ? 'Authenticated' : 'Not Authenticated'"
-                    :title="authUsername">
+            <v-list nav class="bg-navigationMenu">
+                <v-list-item class="py-2" :active="false" nav :subtitle="authStatus" :title="authUsername">
                     <template #prepend>
                         <v-avatar color="surface-light" icon="mdi-account" rounded>
                             <v-icon color="medium-emphasis" />
@@ -24,7 +26,7 @@
                     </template>
                 </v-list-item>
             </v-list>
-            <template #actions>
+            <template v-if="authStore.getAuthStatus" #actions>
                 <v-spacer></v-spacer>
                 <v-btn append-icon="mdi-logout" class="text-none" color="primary" text="Logout" @click="logout" />
             </template>
@@ -43,14 +45,18 @@
 
     // Computed properties
     const authStatus = computed(() =>
-        authStore.getAuthStatus ? 'Authenticated' : envStore.getBasicAuthActive ? '' : 'Not Authenticated'
+        authStore.getAuthStatus
+            ? 'Authenticated'
+            : envStore.getBasicAuthActive
+              ? 'Basic Authentication active'
+              : 'Not Authenticated'
     );
     const isAuthEnabled = computed(() => authStore.getAuthEnabled || envStore.getBasicAuthActive);
     const authUsername = computed(
         () => authStore.getUsername || (envStore.getBasicAuthActive ? envStore.getBasicAuthUsername : '')
     );
 
-    function logout() {
+    function logout(): void {
         authStore.getKeycloak?.logout();
         const refreshIntervalId = authStore.getRefreshIntervalId;
         if (refreshIntervalId) {
