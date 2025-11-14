d16168f5bba239b59587889a078b8bf789c6d1a0
David Ehrhardt <d.ehrhardt@proton.me>
2025-11-14 15:55:26 +0100

    add filter button

---
+++ SOURCE/aas-web-ui/src/components/AppNavigation/FilterAAS.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-menu :close-on-content-click="false" location="bottom">
+        <template #activator="{ props }">
+            <v-btn icon="mdi-filter" v-bind="Object.assign({}, props, tooltipProps)" variant="plain"></v-btn>
+        </template>
+        <v-card>
+            <v-list>
+                <v-list-item>
+                    <v-checkbox v-model="filter" label="Filter"></v-checkbox>
+                    <v-checkbox v-model="filter2" label="Filter 2"></v-checkbox>
+                </v-list-item>
+            </v-list>
+        </v-card>
+    </v-menu>
+</template>
