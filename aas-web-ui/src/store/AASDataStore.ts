3ed1a127626399ff5cb7f7fd9b61b171ea65225a
Aaron Zielstorff <aaron.zielstorff@iese.fraunhofer.de>
2025-12-24 13:12:58 +0100

    Reverts changes in store and moves setup to examples

--- aas-web-ui/src/store/AASDataStore.ts
+++ aas-web-ui/src/store/AASDataStore.ts
@@ -1,58 +1,57 @@
 import { defineStore } from 'pinia';
 
-export const useAASStore = (id: string = 'aasStore') =>
-    defineStore(id, () => {
-        // State
-        const aasObject = ref({} as any); // holds the AAS object for the currently selected AAS
-        const selectedNode = ref({} as any); // holds the currently selected Node in the SubmodelTree/List Component
-        const initTreeByReferenceElement = ref(false); // holds the state if the SubmodelTree Component should be initialized (e.g. cause of clicking Jump-Button of a ReferenceElement)
+export const useAASStore = defineStore('aasStore', () => {
+    // States
+    const aasObject = ref({} as any); // holds the AAS object for the currently selected AAS
+    const selectedNode = ref({} as any); // holds the currently selected Node in the SubmodelTree/List Component
+    const initTreeByReferenceElement = ref(false); // holds the state if the SubmodelTree Component should be initialized (e.g. cause of clicking Jump-Button of a ReferenceElement)
 
+    // Getters
+    const getSelectedAAS = computed(() => aasObject.value);
+    const getSelectedNode = computed(() => selectedNode.value);
+    const getInitTreeByReferenceElement = computed(() => initTreeByReferenceElement.value);
+
+    // Actions
+    function dispatchSelectedAAS(aasValue: any): void {
+        if (
+            aasObject.value &&
+            Object.keys(aasObject.value).length > 0 &&
+            aasObject.value?.id &&
+            aasValue &&
+            Object.keys(aasValue).length > 0 &&
+            aasValue?.id
+        ) {
+            // If existing AAS is replaced by another one, clear selectedNode
+            if (aasObject.value?.id !== aasValue?.id) selectedNode.value = {};
+        }
+
+        if (!aasValue || Object.keys(aasValue).length === 0) {
+            // If empty AAS is dispatched, clear selectedNode
+            selectedNode.value = {};
+        }
+
+        // If the same AAS is dispatched, nothing happened with the selectedNode
+
+        aasObject.value = aasValue;
+    }
+
+    function dispatchSelectedNode(selectedNodeValue: any): void {
+        selectedNode.value = selectedNodeValue;
+    }
+
+    function dispatchInitTreeByReferenceElement(initTreeByReferenceElementValue: boolean): void {
+        initTreeByReferenceElement.value = initTreeByReferenceElementValue;
+    }
+
+    return {
         // Getters
-        const getSelectedAAS = computed(() => aasObject.value);
-        const getSelectedNode = computed(() => selectedNode.value);
-        const getInitTreeByReferenceElement = computed(() => initTreeByReferenceElement.value);
+        getSelectedAAS,
+        getSelectedNode,
+        getInitTreeByReferenceElement,
 
         // Actions
-        function dispatchSelectedAAS(aasValue: any): void {
-            if (
-                aasObject.value &&
-                Object.keys(aasObject.value).length > 0 &&
-                aasObject.value?.id &&
-                aasValue &&
-                Object.keys(aasValue).length > 0 &&
-                aasValue?.id
-            ) {
-                // If existing AAS is replaced by another one, clear selectedNode
-                if (aasObject.value?.id !== aasValue?.id) selectedNode.value = {};
-            }
-
-            if (!aasValue || Object.keys(aasValue).length === 0) {
-                // If empty AAS is dispatched, clear selectedNode
-                selectedNode.value = {};
-            }
-
-            // If the same AAS is dispatched, nothing happened with the selectedNode
-
-            aasObject.value = aasValue;
-        }
-
-        function dispatchSelectedNode(selectedNodeValue: any): void {
-            selectedNode.value = selectedNodeValue;
-        }
-
-        function dispatchInitTreeByReferenceElement(initTreeByReferenceElementValue: boolean): void {
-            initTreeByReferenceElement.value = initTreeByReferenceElementValue;
-        }
-
-        return {
-            // Getters
-            getSelectedAAS,
-            getSelectedNode,
-            getInitTreeByReferenceElement,
-
-            // Actions
-            dispatchSelectedAAS,
-            dispatchSelectedNode,
-            dispatchInitTreeByReferenceElement,
-        };
-    })();
+        dispatchSelectedAAS,
+        dispatchSelectedNode,
+        dispatchInitTreeByReferenceElement,
+    };
+});
