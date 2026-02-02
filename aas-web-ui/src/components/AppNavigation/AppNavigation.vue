3849c4f4339b3851ef87cde51382ad708d516b74
Rene Fischer <33397768+FischerRene@users.noreply.github.com>
2026-02-02 10:00:37 +0100

    Add support for URLs as logo image source (#904)
    * add support for URLs as logo image source
    
    * update Rene + add Roche as contributor
    
    * add Logo path changes to readme
    
    * add missing type

--- aas-web-ui/src/components/AppNavigation/AppNavigation.vue
+++ aas-web-ui/src/components/AppNavigation/AppNavigation.vue
@@ -336,9 +336,13 @@
         }
 
         if (isDark.value && envStore.getEnvLogoDarkPath.trim().length > 0) {
-            return logoFolder + envStore.getEnvLogoDarkPath;
+            return validURL(envStore.getEnvLogoDarkPath)
+                ? envStore.getEnvLogoDarkPath
+                : logoFolder + envStore.getEnvLogoDarkPath;
         } else {
-            return logoFolder + envStore.getEnvLogoLightPath;
+            return validURL(envStore.getEnvLogoLightPath)
+                ? envStore.getEnvLogoLightPath
+                : logoFolder + envStore.getEnvLogoLightPath;
         }
     });
     const showMobileMenu = computed(() => isMobile.value && !mainMenu.value);
@@ -415,5 +419,18 @@
     function openInfrastructureManagement(): void {
         infrastructureMenu.value = false;
         infrastructureManagementDialog.value = true;
+    }
+
+    function validURL(str: string) {
+        var pattern = new RegExp(
+            '^(https?:\\/\\/)?' + // protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+                '(\\#[-a-z\\d_]*)?$',
+            'i'
+        ); // fragment locator
+        return !!pattern.test(str);
     }
 </script>
