cfbddcca7976c54b8a583781312290649f915f7d
Rene Fischer <33397768+FischerRene@users.noreply.github.com>
2026-02-09 14:59:38 +0100

    Update URL validation
    removed regex and added typescript URL checker instead

--- aas-web-ui/src/components/AppNavigation/AppNavigation.vue
+++ aas-web-ui/src/components/AppNavigation/AppNavigation.vue
@@ -422,15 +422,12 @@
     }
 
     function validURL(str: string) {
-        var pattern = new RegExp(
-            '^(https?:\\/\\/)?' + // protocol
-                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-                '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-                '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-                '(\\#[-a-z\\d_]*)?$',
-            'i'
-        ); // fragment locator
-        return !!pattern.test(str);
+        try {
+            const url = new URL(str);
+            // Ensure we only accept web protocols (http/https)
+            return url.protocol === "http:" || url.protocol === "https:";
+        } catch (_) {
+            return false;
+        }
     }
 </script>
