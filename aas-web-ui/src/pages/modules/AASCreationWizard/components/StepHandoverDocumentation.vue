c2cea750ea89dde45948b3c731a5c5fd50f30dd6
Prajwala Adiga <prajwala.adiga@iese.fraunhofer.de>
2026-04-20 14:51:50 +0200

    isolate the final form submit composable

--- aas-web-ui/src/pages/modules/AASCreationWizard/components/StepHandoverDocumentation.vue
+++ aas-web-ui/src/pages/modules/AASCreationWizard/components/StepHandoverDocumentation.vue
@@ -2,15 +2,12 @@
   <div> HandoverDocumentation Form</div>
   <div class="d-flex justify-space-between">
     <v-btn color="primary" @click="props.prev">Back</v-btn>
-    <v-btn color="primary" @click="submitAll">Submit</v-btn>
+    <v-btn color="primary" @click="handleSubmit">Submit</v-btn>
   </div>
 </template>
 <script lang="ts" setup>
-  import { jsonization } from '@aas-core-works/aas-core3.1-typescript'
-  import { useAASRepositoryClient } from '@/composables/Client/AASRepositoryClient'
-  import { buildAssetAdministrationShell } from '../builders/buildAssetAdministrationShell'
+  import { useAASCreationSubmission } from '../composables/useAASCreationSubmission'
   import { useAASCreationStore } from '../stores/aasCreationForm'
-  import { useSMRepositoryClient } from './../../../../composables/Client/SMRepositoryClient'
 
   const props = defineProps<{
     next: () => void
@@ -20,67 +17,17 @@
   }>()
 
   const store = useAASCreationStore()
-  const { postAas } = useAASRepositoryClient()
-  const { postSubmodel } = useSMRepositoryClient()
 
-  async function submitAll (): Promise<void> {
+  const { submitAll } = useAASCreationSubmission()
+
+  async function handleSubmit (): Promise<void> {
     if (!props.isActiveComponent) {
       return
     }
-
-    const assetData = store.assetData
-    const digitalNameplate = store.digitalNameplateData
-
-    if (!digitalNameplate) {
-      console.error('Digital Nameplate data is missing')
+    const success = await submitAll()
+    if (!success) {
+      window.alert('Submission failed')
       return
-    }
-    try {
-      const submodelParseResult = jsonization.submodelFromJsonable(digitalNameplate as any)
-
-      if (submodelParseResult.error !== null) {
-        console.error('Error parsing Submodel:', submodelParseResult.error)
-        return
-      }
-      const submodelInstance = submodelParseResult.mustValue()
-
-      const submodelSuccess = await postSubmodel(submodelInstance)
-
-      console.log('post was a success', submodelSuccess)
-
-      if (!submodelSuccess) {
-        console.log('post function failed')
-
-        return
-      }
-    } catch (error) {
-      console.log('unexpected submodel submission error', error)
-      window.alert('There was an error creating submodel: ${String(error)}')
-    }
-
-    const builtAas = buildAssetAdministrationShell(assetData, digitalNameplate)
-    console.log('builtAas', builtAas)
-
-    try {
-      const aasParseResult = jsonization.assetAdministrationShellFromJsonable(builtAas as any)
-      if (aasParseResult.error !== null) {
-        console.error('Error parsing AAS:', aasParseResult.error)
-        return
-      }
-      const aasInstance = aasParseResult.mustValue()
-
-      const success = await postAas(aasInstance)
-
-      console.log('aas post was a success', success)
-      if (!success) {
-        console.log('AAS post failed')
-        return
-      }
-
-      console.log('AAS post succeeded')
-    } catch (error) {
-      console.log('unexpected aas submission error', error)
-      window.alert('There was an error creating aas: ${String(error)}')
     }
 
     window.alert('Submission was successful.')
