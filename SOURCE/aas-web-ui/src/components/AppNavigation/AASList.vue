e05b59fecfd50ab76a058e042d8d33d924d9f6ce
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2026-04-23 22:21:18 +0200

    preserve sorting options

--- SOURCE/aas-web-ui/src/components/AppNavigation/AASList.vue
+++ SOURCE/aas-web-ui/src/components/AppNavigation/AASList.vue
@@ -96,7 +96,7 @@
                         </v-col>
                         <!-- AAS Filter -->
                         <v-col cols="auto" class="px-0">
-                            <FilterAAS @update:sort="sortAasList" @update:filters="onAttributeFiltersChange" />
+                            <FilterAAS @update:sort="setSortOptions" @update:filters="onAttributeFiltersChange" />
                         </v-col>
                     </v-row>
                 </v-card-title>
@@ -373,6 +373,8 @@
     // Data
     const aasList = ref([] as Array<any>) as Ref<Array<any>>; // Variable to store the AAS Data (AAS or AAS Descriptors)
     const aasListUnfiltered = ref([] as Array<any>) as Ref<Array<any>>; // Variable to store the AAS Data before filtering
+    const sortField = ref('name');
+    const sortDirection = ref(1);
     const searchInput = ref('');
     const attributeFilters = ref<AASAttributeFilters>({
         manufacturerName: '',
@@ -1001,7 +1003,7 @@ const hasGlobalMatch = (searchTerm: string) => {
                 enrichAttributeFields(processedList);
             }
 
-            sortAasList({ sortField: 'name', sortDirection: 1 });
+            sortAasList();
             applyListFilters();
             scrollToSelectedAAS();
             listLoading.value = false;
@@ -1039,7 +1041,13 @@ const hasGlobalMatch = (searchTerm: string) => {
             });
     }
 
-    function sortAasList({ sortField, sortDirection }) {
+    function setSortOptions(sortOptions) {
+        sortField.value = sortOptions.sortField;
+        sortDirection.value = sortOptions.sortDirection;
+        sortAasList();
+    }
+
+    function sortAasList() {
         const compareFunctions = {
             name: (a, b) => nameToDisplay(a).toLowerCase().localeCompare(nameToDisplay(b).toLowerCase()),
             id: (a, b) => (a.idLower ?? "").localeCompare(b.idLower ?? ""),
@@ -1048,7 +1056,7 @@ const hasGlobalMatch = (searchTerm: string) => {
             createdAt: (a, b) => Date.parse(a.administration.createdAt ?? 0) - Date.parse(b.administration.createdAt ?? 0),
         };
 
-        const sortFunction = (a, b) => compareFunctions[sortField](a, b) * sortDirection;
+        const sortFunction = (a, b) => compareFunctions[sortField.value](a, b) * sortDirection.value;
 
         aasList.value = [...aasList.value].sort(sortFunction);
         aasListUnfiltered.value = [...aasListUnfiltered.value].sort(sortFunction);
