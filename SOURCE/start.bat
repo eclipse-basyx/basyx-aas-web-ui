a13b4b72da01bd7a0d0872a31bb620940a8320b1
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2026-04-24 09:52:54 +0200

    include nameplate generator in start script

--- SOURCE/start.bat
+++ SOURCE/start.bat
@@ -16,6 +16,10 @@ if /i "%clean_install%"=="y" (
     cd /d "%ROOT%aas-spring-backend\basyx.aasenvironment\basyx.aasenvironment.component"
     call mvn clean install -DskipTests
 
+    cd /d "%ROOT%aas-test-backend\nameplate-generator-backend"
+    rmdir /s /q node_modules 2>nul
+    call npm install --legacy-peer-deps
+
     cd /d "%ROOT%aas-web-ui"
     rmdir /s /q node_modules 2>nul
     call yarn install
@@ -29,11 +33,16 @@ cd /d "%ROOT%aas-spring-backend\basyx.aasenvironment\basyx.aasenvironment.compon
 echo Starting Maven Spring Boot...
 start "BaSyxMaven" /b cmd /c "mvn spring-boot:run >nul 2>&1"
 
+cd /d "%ROOT%aas-test-backend\nameplate-generator-backend"
+rmdir /s /q node_modules 2>nul
+start "BaSyxNameplateGenerator" /b cmd /c "npm start >nul 2>&1"
+
 cd "%ROOT%\aas-web-ui"
 echo Starting frontend...
 yarn dev
 
 echo Stopping backend services...
 taskkill /FI "WINDOWTITLE eq BaSyxMaven*" /T /F
+taskkill /FI "WINDOWTITLE eq BaSyxNameplateGenerator*" /T /F
 cd /d "%ROOT%aas-test-backend"
 docker compose down
