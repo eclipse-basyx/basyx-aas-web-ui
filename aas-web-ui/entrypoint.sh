cb6c754b3972c21269530ce211a6cd30746fee92
Sebastian Eicke <sebastian.eicke@HARTING.com>
2026-07-31 11:11:16 +0200

    Remove commented instructions for NODE_USE_ENV_PROXY in entrypoint script

--- aas-web-ui/entrypoint.sh
+++ aas-web-ui/entrypoint.sh
@@ -254,8 +254,6 @@ trap stop_processes INT TERM
 
 if [ "$CX_EDC_BFF_ENABLED" = "true" ]; then
     echo "Starting integrated Catena-X EDC BFF on port $CX_EDC_BFF_PORT"
-    # NODE_USE_ENV_PROXY=1 makes Node.js natively honor HTTP_PROXY/HTTPS_PROXY/NO_PROXY
-    # for outgoing EDC requests (requires Node.js >= 24.5.0 / 22.21.0).
     NODE_USE_ENV_PROXY=1 node /usr/src/app/dist-bff/edc-bff/server.js &
     BFF_PID="$!"
 fi
